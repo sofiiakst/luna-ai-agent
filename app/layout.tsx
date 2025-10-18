3e5fefb4e99ab7586ff50031c7c6541b44941265
sofiiakst <sophikost6@gmail.com>
2025-10-18 18:12:27 +0300

    error fix 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import ConvexClientProvider from "../components/ConvexClientprovider";
+import ConvexClientProvider from "../components/ConvexClientProvider";
 import { Authenticated } from "convex/react";
 import Header from "@/components/Header";
 import { ClerkProvider } from "@clerk/nextjs";
